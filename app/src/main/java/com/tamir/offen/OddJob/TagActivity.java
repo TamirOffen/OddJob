568410fe93b6198d0789c830438dc9a83aa42121
paen3 <vaenugula@gmail.com>
2018-07-30 17:38:24 -0700

    Added: AddActivity+Layout,DateActivity+Layout,LocationPickerActivity+Layout, PriceActivity+Layout, TagActivity+Layout, along with all UI design files Location: Intel Cafe - Phaniraj Aenugula

---
+++ app/src/main/java/com/tamir/offen/OddJob/TagActivity.java
@@ -0,0 +1,13 @@
+package com.tamir.offen.OddJob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag);
+    }
+}
